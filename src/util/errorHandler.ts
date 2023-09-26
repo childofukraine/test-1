b09ca349a3cc5cb6d2d9ce477412f8aaeb32cf7d
childofukraine <sailant686@gmail.com>
2023-09-26 15:37:26 +0300

    main

--- src/util/errorHandler.ts
+++ src/util/errorHandler.ts
@@ -3,6 +3,7 @@ import type { ErrorRequestHandler } from "express";
 
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   if (err instanceof Boom) {
+    console.log('test')
     return res.status(err.output.statusCode).json({ message: err.message });
   }
 };
