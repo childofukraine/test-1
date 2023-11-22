fe74ecb2c83154acce8640789cdb88a449705a84
childofukraine <sailant686@gmail.com>
2023-11-22 18:45:52 +0200

    check

--- src/router/router.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { Controller } from "../controller/contoller";
-
-export const router = express.Router();
-
-router.get("/bye", Controller.hi);
-router.get("/hi", Controller.hi);
-
--- src/util/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import { Boom } from "@hapi/boom";
-import type { ErrorRequestHandler } from "express";
-
-export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  if (err instanceof Boom) {
-    console.log('test')
-    return res.status(err.output.statusCode).json({ message: err.message });
-  }
-};
